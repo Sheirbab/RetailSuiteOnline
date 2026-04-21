23bbfc7f0000d82f837aa277a4064c8856768ea0
mshahriyar <mshahriyar@ublfunds.com>
2026-04-21 15:59:45 +0500

    Added simple POS screen .

---
+++ RetailSuite.Api/Controllers/SalesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RetailSuite.Infrastructure;
+using RetailSuite.Infrastructure.Modules.Inventory.Entities;
+using RetailSuite.Infrastructure.Modules.Orders.Dtos;
+using RetailSuite.Infrastructure.Modules.Orders.Services;
+namespace RetailSuite.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SalesController : ControllerBase
+    {
+        private readonly RetailDbContext _context;
+        private readonly SaleService _saleService;
+        public SalesController(RetailDbContext context, SaleService saleService)
+        {
+            _context = context;
+            _saleService = saleService;
+        }
+        [HttpPost("checkout")]
+        public async Task<IActionResult> Checkout([FromBody] CreatePosSaleRequest request)
+        {
+            var orderId = await _saleService.ProcessPosSaleAsync(request);
+            return Ok(orderId);
+        }
+    }
+}
