4fd51da2c6cfc15a1f90e49da5d4ac01c65a3ede
shehriyar89se <shehriyar89se@gmail.com>
2026-07-13 16:31:33 +0500

    chore: remove empty ProductAttributesController stub

--- RetailSuite.Api/Controllers/ProductAttributesController.cs
+++
@@ -1,7 +0,0 @@
-// This controller was a stale earlier version of AttributesController and has
-// been removed. Its [Route("api/attributes")] with [HttpGet], [HttpPost], and
-// [HttpPost("{attributeId}/values")] collided with the current
-// AttributesController on the same paths, which caused Swashbuckle to throw
-// a 500 when generating /swagger/v1/swagger.json.
-//
-// TODO: `git rm` this file. It is intentionally empty until then.
